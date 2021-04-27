c43c5a2bca46b70976f06af0caf29dc9e7392dad
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-04-27 10:06:12 +0300

    Create MotorcycleController and AddMotorcycleViewModel

---
+++ Web/ClicksAndDrive.Web.ViewModels/Motorcycles/AddMotorcycleViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace ClicksAndDrive.Web.ViewModels
+{
+    using ClicksAndDrive.Data.Models.Enums;
+    using Microsoft.AspNetCore.Http;
+    using System.ComponentModel.DataAnnotations;
+
+    public class AddMotorcycleViewModel
+    {
+        [Required]
+        public MotorcycleType Type { get; set; }
+
+        [Required]
+        [RegularExpression("[A-Z][^_]+", ErrorMessage = "Name should start with upper letter.")]
+        public string Made { get; set; }
+
+        [Required]
+        public TransmissionType Transmission { get; set; }
+
+        [Required]
+        public decimal PriceForHour { get; set; }
+
+
+        [Required]
+        public IFormFile ImageUrl { get; set; }
+
+
+        [MaxLength(250)]
+        public string? Description { get; set; }
+    }
+}
---
+++ Web/ClicksAndDrive.Web/Controllers/MotorcycleController.cs
@@ -0,0 +1,23 @@
+﻿namespace ClicksAndDrive.Web.Controllers
+{
+    using ClicksAndDrive.Web.ViewModels;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class MotorcycleController : Controller
+    {
+        public IActionResult Add()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddMotorcycleViewModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View();
+            }
+            return this.Redirect("/");
+        }
+    }
+}
