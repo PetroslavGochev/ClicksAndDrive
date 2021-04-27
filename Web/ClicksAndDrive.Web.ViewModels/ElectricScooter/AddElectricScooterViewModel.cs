4d025e7d457ef8f0d3f53c5e69f53a62f4947402
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-04-27 16:50:25 +0300

    Create ElectricScooterController and AddElectricScooterViewModel

--- Web/ClicksAndDrive.Web.ViewModels/ElectricScooter/AddElectricScooterViewModel.cs
+++ Web/ClicksAndDrive.Web.ViewModels/ElectricScooter/AddElectricScooterViewModel.cs
@@ -1,30 +1,28 @@
-﻿namespace ClicksAndDrive.Web.ViewModels
+﻿namespace ClicksAndDrive.Web.ViewModels.ElectricScooter
 {
-    using ClicksAndDrive.Data.Models.Enums;
     using Microsoft.AspNetCore.Http;
     using System.ComponentModel.DataAnnotations;
 
-    public class AddMotorcycleViewModel
+    public class AddElectricScooterViewModel
     {
-        [Required]
-        public MotorcycleType Type { get; set; }
-
         [Required]
         [RegularExpression("[A-Z][^_]+", ErrorMessage = "Name should start with upper letter.")]
         public string Made { get; set; }
 
         [Required]
-        public TransmissionType Transmission { get; set; }
+        [Range(5,35)] 
+        public int MaximumSpeed { get; set; }
+
+        [Required]
+        public int Mileage { get; set; }
 
         public bool IsAvailable { get; set; }
 
         [Required]
         public decimal PriceForHour { get; set; }
 
-
         [Required]
         public IFormFile ImageUrl { get; set; }
-
 
         [MaxLength(250)]
         public string? Description { get; set; }
---
+++ Web/ClicksAndDrive.Web/Controllers/ElectricScooterController.cs
@@ -0,0 +1,23 @@
+﻿namespace ClicksAndDrive.Web.Controllers
+{
+    using ClicksAndDrive.Web.ViewModels.ElectricScooter;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ElectricScooterController : Controller
+    {
+        public IActionResult Add()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddElectricScooterViewModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View();
+            }
+            return this.Redirect("/");
+        }
+    }
+}
