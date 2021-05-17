4d182d3a142e8bebb944e937a83a8a836591f7e3
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-05-17 16:29:51 +0300

    Create edit view and edit bicycle view model

---
+++ Web/ClicksAndDrive.Web.ViewModels/Bicycles/EditBicycleViewModel.cs
@@ -0,0 +1,39 @@
+﻿namespace ClicksAndDrive.Web.ViewModels.Bicycles
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using ClicksAndDrive.Data.Models.Enums;
+    using Microsoft.AspNetCore.Http;
+
+    public class EditBicycleViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public BicycleType Type { get; set; }
+
+        [Required]
+        [RegularExpression("[A-Z][^_]+", ErrorMessage = "Name should start with upper letter.")]
+        public string Made { get; set; }
+
+        [Required]
+        public byte Speeds { get; set; }
+
+        [Required]
+        public BicycleSize Size { get; set; }
+
+        [Required]
+        public byte SizeOfTires { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        [Required]
+        public decimal PriceForHour { get; set; }
+
+        //[RegularExpression(@"([^\s]+(\.(?i)(jpg|png|gif|bmp))$)", ErrorMessage ="Invalid type")]
+        public string Image { get; set; }
+
+        [MaxLength(250)]
+        public string Description { get; set; }
+    }
+}
--- Web/ClicksAndDrive.Web/Controllers/BicycleController.cs
+++ Web/ClicksAndDrive.Web/Controllers/BicycleController.cs
@@ -2,6 +2,7 @@
 {
     using ClicksAndDrive.Services.Data;
     using ClicksAndDrive.Web.ViewModels.Bicycles;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
 
     public class BicycleController : Controller
@@ -51,6 +52,38 @@
             var bicycle = this.bicycleService.Details(id);
 
             return this.View(bicycle);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var bicycle = this.bicycleService.Edit(id);
+
+            var model = new EditBicycleViewModel()
+            {
+                Id = bicycle.Id,
+                Type = bicycle.Type,
+                Made = bicycle.Made,
+                Speeds = bicycle.Speeds,
+                Size = bicycle.Size,
+                SizeOfTires = bicycle.SizeOfTires,
+                PriceForHour = bicycle.PriceForHour,
+                Description = bicycle.Description,
+            };
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditBicycleViewModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+
+            this.bicycleService.DoEdit(input);
+
+            return this.Redirect($"/Bicycle/Details/{input.Id}");
         }
     }
 }
