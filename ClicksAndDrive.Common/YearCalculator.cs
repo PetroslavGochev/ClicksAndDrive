a4add95a9c6a468309844bc9ffe7bcd96332302f
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-06-10 22:33:48 +0300

    Create static class YearCalculator

---
+++ ClicksAndDrive.Common/YearCalculator.cs
@@ -0,0 +1,23 @@
+﻿namespace ClicksAndDrive.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public static class YearCalculator
+    {
+        public static byte CalculateYear(DateTime birthday)
+        {
+            DateTime today = DateTime.Today;
+
+            int age = today.Year - birthday.Year;
+
+            if (birthday > today.AddYears(-age))
+            {
+                age--;
+            }
+
+            return (byte)age;
+        }
+    }
+}
