b264c9519e824af44f461b769a9b5606738ae4f3
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-06-12 21:45:39 +0300

    Scaffold personal data

---
+++ Web/ClicksAndDrive.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ClicksAndDrive.Data;
+using ClicksAndDrive.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ClicksAndDrive.Web.Areas.Identity.IdentityHostingStartup))]
+namespace ClicksAndDrive.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
