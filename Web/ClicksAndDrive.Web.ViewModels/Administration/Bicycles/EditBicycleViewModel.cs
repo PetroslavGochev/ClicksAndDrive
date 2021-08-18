d43f96d8e2c7708b0196e51efa50f4b0262fdfa2
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-08-18 17:46:14 +0300

    Modified edit bicycle view model

--- Web/ClicksAndDrive.Web.ViewModels/Administration/Bicycles/EditBicycleViewModel.cs
+++ Web/ClicksAndDrive.Web.ViewModels/Administration/Bicycles/EditBicycleViewModel.cs
@@ -2,6 +2,7 @@
 {
     using System.ComponentModel.DataAnnotations;
 
+    using AutoMapper;
     using ClicksAndDrive.Common;
     using ClicksAndDrive.Data.Models;
     using ClicksAndDrive.Data.Models.Enums;
